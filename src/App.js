746b8859070c096ffe1bbdf3c50c46486b8fdabc
Duaa Tariq <tduaa34@gmail.com>
2024-06-20 18:01:40 -0400

    Updated code with changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FEMCARE</h1> {/* Title */}
+        <h4>....We all care</h4> {/* Subtitle */}
       </header>
+
+      <nav className="Nav-bar">
+       {/*Add nav bar here */}
+      </nav>
+
+      <main className = "Main-content">
+        <div className="image-container">
+          <img src="/American Medical Association image.jpeg" alt="taken from American Medical Association" />
+        </div>
+        <section>
+          <div className="latest">
+
+          </div>
+          <div className="new-moms-quicklink">
+
+          </div>
+          <div className="chat-bot">
+
+          </div>
+        </section>
+      </main>
+      
     </div>
   );
 }
 
 export default App;
+
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
